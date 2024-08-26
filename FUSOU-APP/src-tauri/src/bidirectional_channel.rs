6e7ed384b166a48228ee33bee4ad9353d09ba9b5
Oguri Hideyuki <ogu.hide.u@gmail.com>
2024-08-27 04:55:21 +0900

    implement simple bidirectional channel

---
+++ FUSOU-APP/src-tauri/src/bidirectional_channel.rs
@@ -0,0 +1,87 @@
+use std::sync::{Arc, Mutex};
+
+use tokio::sync::mpsc;
+
+#[derive(Debug, Clone)]
+pub enum StatusInfo {
+    HEALTH {
+        status: String,
+        message: String,
+    },
+    SHUTDOWN {
+        status: String,
+        message: String,
+    },
+}
+
+pub struct  Master<T> where T: Clone {
+    pub tx: mpsc::Sender<T>,
+    pub rx: Arc<Mutex<mpsc::Receiver<T>>>,
+}
+
+
+impl<T> Master<T> where T: Clone {
+    pub async fn send(&self, message: T) -> Result<(), mpsc::error::SendError<T>> {
+        self.tx.send(message).await
+    }
+    pub async fn recv(&mut self) -> Option<T> {
+        match self.rx.lock() {
+            Ok(mut rx) => rx.recv().await,
+            Err(_) =>None,
+        }
+    }
+}
+
+pub struct Slave<T> where T: Clone {
+    pub tx: mpsc::Sender<T>,
+    pub rx: Arc<Mutex<mpsc::Receiver<T>>>,
+}
+
+impl <T> Slave<T> where T: Clone {
+    pub async fn send(&self, message: T) -> Result<(), mpsc::error::SendError<T>> {
+        self.tx.send(message).await
+    }
+    pub async fn recv(&mut self) -> Option<T> {
+        match self.rx.lock() {
+            Ok(mut rx) => rx.recv().await,
+            Err(_) => None,
+        }
+    }
+    
+}
+
+pub struct BidirectionalChannel<T> where T: Clone {
+    pub master: Master<T>,
+    pub slave: Slave<T>,
+}
+
+impl<T> BidirectionalChannel<T> where T: Clone {
+    pub fn new(buffer: usize) -> Self {
+        let (master_tx, slave_rx) = mpsc::channel::<T>(buffer);
+        let (slave_tx, master_rx) = mpsc::channel::<T>(buffer);
+        let master = Master {
+            tx: master_tx,
+            rx: Arc::new(Mutex::new(master_rx)),
+        };
+        let slave = Slave {
+            tx: slave_tx,
+            rx: Arc::new(Mutex::new(slave_rx)),
+        };
+        BidirectionalChannel {
+            master,
+            slave,
+        }
+    }
+    pub fn clone_master(&self) -> Master<T> {
+        Master {
+            tx: self.master.tx.clone(),
+            rx: self.master.rx.clone(),
+        }
+    }
+    pub fn clone_slave(&self) -> Slave<T> {
+        Slave {
+            tx: self.slave.tx.clone(),
+            rx: self.slave.rx.clone(),
+        }
+    }
+}
