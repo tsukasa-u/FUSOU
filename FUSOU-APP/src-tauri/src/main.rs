a3698717d6348b38fac8157f00bdd26ac9eccd93
Oguri Hideyuki <ogu.hide.u@gmail.com>
2024-08-27 19:17:18 +0900

    Update main.rs
    add proxy start and shutdown
    add wait group

--- FUSOU-APP/src-tauri/src/main.rs
+++ FUSOU-APP/src-tauri/src/main.rs
@@ -4,17 +4,17 @@
 
 use tauri::{AppHandle, CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu};
 use tauri::Manager;
-use tauri::api::notification::Notification;
 use tokio::sync::mpsc;
-use tokio::sync::broadcast;
 use webbrowser::{open_browser, Browser};
 use arboard::Clipboard;
 use std::sync::{Arc, Mutex};
 use std::process::ExitCode;
-use crossbeam_utils::sync::WaitGroup;
+use wg::AsyncWaitGroup;
 
 mod kcapi;
 mod notification;
+
+use proxy::bidirectional_channel::{BidirectionalChannel, Master, Slave, StatusInfo};
 
 #[derive(Debug, Default )]
 pub struct BrowserState(Browser);
@@ -34,11 +34,11 @@ impl BrowserState {
     }
 }
 
+#[tokio::main]
+async fn main() -> ExitCode {
 
-fn main() -> ExitCode {
-
-  let _wg: WaitGroup = WaitGroup::new();
-  let wg = _wg.clone();
+  let wg = AsyncWaitGroup::new();
+  // let wg = _wg.clone();
 
   // let (tx_master_gprc, rx_slave_gprc) = mpsc::channel::<gprc_status::StatusInfo>(1);
   // let (tx_slave_gprc, rx_master_gprc) = mpsc::channel::<gprc_status::StatusInfo>(1);
@@ -47,12 +47,24 @@ fn main() -> ExitCode {
   // let _ = gprc_server::gprc_start_with_thread(_wg.clone(), tx_slave_gprc, rx_slave_gprc, addr);
   //
 
+  let proxy_bidirectional_channel = BidirectionalChannel::<StatusInfo>::new(1);
+  let proxy_bidirectional_channel_slave = proxy_bidirectional_channel.clone_slave();
+  let proxy_bidirectional_channel_master = proxy_bidirectional_channel.clone_master();
+
+  let pac_bidirectional_channel = BidirectionalChannel::<StatusInfo>::new(1);
+  let pac_bidirectional_channel_slave = pac_bidirectional_channel.clone_slave();
+  let pac_bidirectional_channel_master = pac_bidirectional_channel.clone_master();
+
+  let (proxy_log_channel_tx, proxy_log_channel_rx) = mpsc::channel::<Vec<u8>>(1);
+
   let shared_browser = Arc::new(Mutex::new(BrowserState::new()));
 
   let proxy_serve_shutdown: CustomMenuItem = CustomMenuItem::new("proxy-serve-shutdown".to_string(), "Shutdown Proxy Server".to_string());
   let gprc_serve_shutdown: CustomMenuItem = CustomMenuItem::new("gprc-serve-shutdown".to_string(), "Shutdown gRPC Server".to_string()).disabled();
   let pac_server_shutdown: CustomMenuItem = CustomMenuItem::new("pac-serve-shutdown".to_string(), "Shutdown PAC Server".to_string());
   let delete_registry: CustomMenuItem = CustomMenuItem::new("delete-registry".to_string(), "Delete Registry".to_string());
+
+  let restart_proxy: CustomMenuItem = CustomMenuItem::new("restart-proxy".to_string(), "Restart Proxy Server".to_string());
 
   let quit: CustomMenuItem = CustomMenuItem::new("quit".to_string(), "Quit".to_string()).accelerator("CmdOrCtrl+Q".to_string());
   let pause: CustomMenuItem = CustomMenuItem::new("pause".to_string(), "Pause".to_string()).selected();
@@ -96,8 +108,26 @@ fn main() -> ExitCode {
   let system_tray = SystemTray::new().with_menu(tray_menu).with_tooltip("FUSOU");
 
   tauri::Builder::default()
-    .setup(|app| {
+    .setup(move |app| {
       // let _window = app.get_window("main").unwrap().close().unwrap();
+
+      // start proxy server
+      let proxy_addr = proxy::proxy_server::serve_proxy("http://localhost:8080".to_string(), 0, proxy_bidirectional_channel_slave, proxy_log_channel_tx.clone());
+
+      if proxy_addr.is_err() {
+        return Err("Failed to start proxy server".into());
+      }
+
+      // start pac server
+      let pac_addr = proxy::pac_server::serve_pac_file("proxy.pac".to_string(), 0, pac_bidirectional_channel_slave);
+      
+      if pac_addr.is_err() {
+        return Err("Failed to start pac server".into());
+      }
+
+      proxy::edit_pac::edit_pac("proxy.pac", proxy_addr.unwrap().to_string().as_str());
+      proxy::cmd_pac::add_pac(&format!("http://localhost:{}/proxy.pac", pac_addr.unwrap().port()));
+
       return Ok(())
     })
     .system_tray(system_tray)
@@ -231,14 +261,30 @@ fn main() -> ExitCode {
     })
     .build(tauri::generate_context!())
     .expect("error while building tauri application")
-    .run(|_app_handle, event| match event {
-      tauri::RunEvent::ExitRequested { api, .. } => {
-        api.prevent_exit();
+    .run(move |_app_handle, event| {
+      match event {
+        tauri::RunEvent::ExitRequested { api, .. } => {
+
+          let pac_bidirectional_channel_master_clone = pac_bidirectional_channel_master.clone();
+          let proxy_bidirectional_channel_master_clone = proxy_bidirectional_channel_master.clone();
+
+          tokio::task::spawn(async {
+            proxy::cmd_pac::remove_pac();
+  
+            let _ = tokio::join!(
+              proxy::bidirectional_channel::request_shutdown(pac_bidirectional_channel_master_clone),
+              proxy::bidirectional_channel::request_shutdown(proxy_bidirectional_channel_master_clone)
+            );
+          });
+
+
+          api.prevent_exit();
+        },
+        _ => {}
       }
-      _ => {}
     });
 
-    _wg.wait();
+    wg.wait().await;
 
     return ExitCode::SUCCESS;
 }
