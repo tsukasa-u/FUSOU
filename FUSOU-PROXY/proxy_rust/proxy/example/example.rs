bd714d198e34fef1b863d20ec56d31c2aa507580
Oguri Hideyuki <ogu.hide.u@gmail.com>
2024-08-27 19:17:31 +0900

    remove await

--- FUSOU-PROXY/proxy_rust/proxy/example/example.rs
+++ FUSOU-PROXY/proxy_rust/proxy/example/example.rs
@@ -14,8 +14,8 @@ async fn main () {
     let (_tx_pac, rx_pac) = oneshot::channel::<()>();
     let (tx_proxy_log, _rx_proxy_log) = tokio::sync::mpsc::channel::<Vec<u8>>(32);
 
-    let proxy_server_addr = proxy_server::serve_proxy(proxy_address.to_string(), 0, rx_proxy, tx_proxy_log).await.unwrap();
-    let pac_server_addr = pac_server::serve_pac_file("proxy.pac".to_string(), 0, rx_pac).await.unwrap();
+    let proxy_server_addr = proxy_server::serve_proxy(proxy_address.to_string(), 0, rx_proxy, tx_proxy_log).unwrap();
+    let pac_server_addr = pac_server::serve_pac_file("proxy.pac".to_string(), 0, rx_pac).unwrap();
 
     edit_pac::edit_pac("proxy.pac", proxy_server_addr.to_string().as_str());
     cmd_pac::add_pac(&format!("http://localhost:{}/proxy.pac", pac_server_addr.port()));
