26a774cf205d356003fe4cf2815b135e6ae45b12
Oguri Hideyuki <ogu.hide.u@gmail.com>
2024-08-29 16:48:24 +0900

    Update cmd_pac.rs

--- FUSOU-PROXY/proxy_rust/proxy/src/cmd_pac.rs
+++ FUSOU-PROXY/proxy_rust/proxy/src/cmd_pac.rs
@@ -2,10 +2,11 @@ use std::process::Command;
     
 #[cfg(target_os = "windows")]
 pub fn add_pac(path: &str) {
-    Command::new("./add_proxy.bat")
+    let _output = Command::new("./add_proxy.bat")
         .arg(path)
         .output()
         .expect("failed to execute process");
+    println!("register AutoConfigURL: {}", path);
 }
     
 #[cfg(target_os = "windows")]
@@ -13,4 +14,5 @@ pub fn remove_pac() {
     Command::new("./delete_proxy.bat")
         .output()
         .expect("failed to execute process");
+    println!("unregister AutoConfigURL");
 }
