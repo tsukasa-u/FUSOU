e6d54855fb7d5e7f77edfa962b17851203bd1875
Oguri Hideyuki <ogu.hide.u@gmail.com>
2024-09-13 09:45:14 +0900

    add detection for url changed

--- FUSOU-APP/src/init_script.js
+++ FUSOU-APP/src/init_script.js
@@ -1,21 +1,54 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    document.getElementsByTagName("html")[0].setAttribute("style", "overflow: hidden;");
-
-    document.getElementsByClassName("dmm-ntgnavi")[0].setAttribute("style", "display: none;");
-    document.getElementById("foot").setAttribute("style", "display: none;");
-    document.getElementById("ntg-recommend").setAttribute("style", "display: none;");
-    document.getElementsByClassName("area-naviapp")[0].setAttribute("style", "display: none;");
     
-    document.getElementById("game_frame").setAttribute("style", "position: fixed; margin: 0; margin-right: auto; margin-left: auto; top: -24px; left: -8px; transform-origin: 8px 24px;");
-    document.getElementById("game_frame").style.transform = "scale(" + window.innerHeight/712 + ", " + window.innerHeight/712 + ")";
+    observer.observe(window.document.body, {
+        subtree: true,
+        childList: true, 
+        attributes: true,
+        characterData: true
+    });
 
+    let html_element = document.getElementsByTagName("html")[0];
+    if (html_element) html_element.setAttribute("style", "overflow: hidden;");
+
+    let dmm_ntgnavi = document.getElementsByClassName("dmm-ntgnavi")[0];
+    if (dmm_ntgnavi) dmm_ntgnavi.setAttribute("style", "display: none;");
+
+    let foot = document.getElementById("foot");
+    if (foot) foot.setAttribute("style", "display: none;");
+
+    let ntg_recommend = document.getElementById("ntg-recommend");
+    if (ntg_recommend) ntg_recommend.setAttribute("style", "display: none;");
+
+    let area_naviapp = document.getElementsByClassName("area-naviapp")[0];
+    if (area_naviapp) area_naviapp.setAttribute("style", "display: none;");
+    
+    let game_frame = document.getElementById("game_frame");
+    if (game_frame) {
+        game_frame.setAttribute("style", "position: fixed; margin: 0; margin-right: auto; margin-left: auto; top: -24px; left: -8px; transform-origin: 8px 24px;");
+        game_frame.style.transform = "scale(" + window.innerHeight/712 + ", " + window.innerHeight/712 + ")";
+    }
 });
 
 window.addEventListener("resize", (event) => {
-    document.getElementById("game_frame").style.transform = "scale(" + window.innerHeight/712 + ", " + window.innerHeight/712 + ")";
+    
+    let game_frame = document.getElementById("game_frame");
+    if (game_frame) game_frame.style.transform = "scale(" + window.innerHeight/712 + ", " + window.innerHeight/712 + ")";
 });
 
 
 window.addEventListener("contextmenu", async (e) => {
     e.preventDefault();
+});
+
+let oldUrl = '';
+
+const observer = new MutationObserver(() => {
+    if(oldUrl !== location.href) {
+        window.dispatchEvent(new CustomEvent('urlChange'));
+        oldUrl = location.href;
+   }
+});
+
+window.addEventListener('urlChange', () => {
+    console.log('URL changed', location.href);
 });
