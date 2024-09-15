b36bd648c33ed865a60ff02403c8182cdab14c63
Oguri Hideyuki <ogu.hide.u@gmail.com>
2024-09-15 20:04:59 +0900

    Update ship.rs

--- FUSOU-APP/src-tauri/src/interface/ship.rs
+++ FUSOU-APP/src-tauri/src/interface/ship.rs
@@ -1,15 +1,16 @@
+use std::collections::HashMap;
+
 use crate::kcapi;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct Ships {
-    ships: Vec<Ship>
+    ships: HashMap<i64, Ship>
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct Ship {
     pub id:         i64,
     pub ship_id:    i64,
-    // pub ship_name:  String,
     pub lv:         i64,      // レベル
     pub exp:        Vec<i64>,    
     pub nowhp:      i64,      // 現在HP
@@ -20,9 +21,7 @@ pub struct Ship {
     pub onsolot:    Vec<i64>,    // 艦載機搭載数
     pub slot_ex:    i64,      // 補強増設
     pub fuel:       i64,      // 燃料
-    // pub max_fuel:   i64,      // 最大燃料
     pub bull:       i64,      // 弾薬
-    // pub max_bull:   i64,      // 最大弾薬
     pub slotnum:    i64,      // 装備スロット数
     pub cond:       i64,      // 疲労度
     pub karyoku:    Vec<i64>,      // 火力
@@ -38,12 +37,12 @@ pub struct Ship {
 
 impl From<Vec<kcapi::api_port::port::ApiShip>> for Ships {
     fn from(ships: Vec<kcapi::api_port::port::ApiShip>) -> Self {
-        let mut ship_list = Vec::new();
+        let mut ship_map = HashMap::new();
         for ship in ships {
-            ship_list.push(ship.into())
+            ship_map.insert(ship.api_id, ship.into());
         }
         Self {
-            ships: ship_list
+            ships: ship_map
         }
     }
 }
@@ -78,3 +77,34 @@ impl From<kcapi::api_port::port::ApiShip> for Ship {
         }
     }
 }
+
+// impl From<kcapi::api_req_sortie::battle::ApiData> for Ships {
+//     fn from(ship: kcapi::api_req_sortie::battle::ApiData) -> Self {
+//         Self {
+//             id:         ship.api_id,
+//             ship_id:    ship.api_ship_id,
+//             lv:         ship.api_lv,
+//             exp:        ship.api_exp,
+//             nowhp:      ship.api_nowhp,
+//             maxhp:      ship.api_maxhp,
+//             soku:       ship.api_soku,
+//             leng:       ship.api_leng,
+//             slot:       ship.api_slot,
+//             onsolot:    ship.api_onslot,
+//             slot_ex:    ship.api_slot_ex,
+//             fuel:       ship.api_fuel,
+//             bull:       ship.api_bull,
+//             slotnum:    ship.api_slotnum,
+//             cond:       ship.api_cond,
+//             karyoku:    ship.api_karyoku,
+//             raisou:     ship.api_raisou,
+//             taiku:      ship.api_taiku,
+//             soukou:     ship.api_soukou,
+//             kaihi:      ship.api_kaihi,
+//             taisen:     ship.api_taisen,
+//             sakuteki:   ship.api_sakuteki,
+//             lucky:      ship.api_lucky,
+//             sally_area: ship.api_sally_area,
+//         }
+//     }
+// }
