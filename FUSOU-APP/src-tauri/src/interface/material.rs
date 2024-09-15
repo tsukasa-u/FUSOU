202176acdde18870b98f73acdd6f51c30a06ee19
Oguri Hideyuki <ogu.hide.u@gmail.com>
2024-09-15 20:05:05 +0900

    Update material.rs

--- FUSOU-APP/src-tauri/src/interface/material.rs
+++ FUSOU-APP/src-tauri/src/interface/material.rs
@@ -1,16 +1,17 @@
+use std::collections::HashMap;
+
 use crate::kcapi;
-use register_trait::TraitForEmitData;
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct Materials {
-    pub materials: Vec<i64>,
+    pub materials: HashMap<usize, i64>,
 }
 
 impl From<Vec<kcapi::api_port::port::ApiMaterial>> for Materials {
     fn from(materials: Vec<kcapi::api_port::port::ApiMaterial>) -> Self {
-        let mut ret = Vec::<i64>::with_capacity(8);
-        for material in materials {
-            ret.push(material.api_value);
+        let mut ret = HashMap::with_capacity(8);
+        for idx in 0..8 {
+            ret.insert(idx, materials[idx].api_value);
         }
 
         Self {
