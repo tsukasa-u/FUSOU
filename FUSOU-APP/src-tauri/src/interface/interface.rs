d628c76ff2592cd660c84af005ca00ef412166cc
Oguri Hideyuki <ogu.hide.u@gmail.com>
2024-09-15 20:05:08 +0900

    Update interface.rs

--- FUSOU-APP/src-tauri/src/interface/interface.rs
+++ FUSOU-APP/src-tauri/src/interface/interface.rs
@@ -1,17 +1,38 @@
-use super::material::Materials;
-use super::logs::Logs;
-use super::deck_port::DeckPorts;
-use super::n_dock::NDocks;
-use super::ship::Ships;
+pub use super::material::Materials;
+pub use super::logs::Logs;
+pub use super::deck_port::DeckPorts;
+pub use super::n_dock::NDocks;
+pub use super::ship::Ships;
+pub use super::mst_ship::MstShips;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub enum EmitData {
+    Add(Add),
+    Set(Set),
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub enum Add {
     Materials(Materials),
     DeckPorts(DeckPorts),
-    // Mission(Mission),
+    // Mission,
     NDocks(NDocks),
     Ships(Ships),
     Logs(Logs),
-    // AirBase(AirBase),
+    // AirBase,(AirBase),
     // Battle(Battle),
+    MstShips(MstShips),
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub enum Set {
+    Materials(Materials),
+    DeckPorts(DeckPorts),
+    // Mission,
+    NDocks(NDocks),
+    Ships(Ships),
+    Logs(Logs),
+    // AirBase,(AirBase),
+    // Battle(Battle),
+    MstShips(MstShips),
 }
