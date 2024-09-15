cc240617cdb9f000206f10bbccc2a1c01f45b486
Oguri Hideyuki <ogu.hide.u@gmail.com>
2024-09-15 20:02:58 +0900

    keep

--- FUSOU-APP/src/components/expedition.tsx
+++ FUSOU-APP/src/components/expedition.tsx
@@ -1,9 +1,9 @@
 import { Slot, component$, useStylesScoped$, useTask$ } from '@builder.io/qwik';
 
-import { DeckPort } from "./interface/port.tsx";
+import { DeckPorts } from "./interface/port.tsx";
 
 interface ExpeditionProps {
-    deckPort: DeckPort[];
+    deckPort: DeckPorts;
 }
  
 export const Expedition = component$<ExpeditionProps>(({ deckPort }) => {
@@ -25,11 +25,11 @@ export const Expedition = component$<ExpeditionProps>(({ deckPort }) => {
     useTask$(() => {
         const interval = setInterval(() => {
             const now_date = Math.floor(Date.now());
-            deckPort.forEach((_, key) => {
-                if (deckPort[key].mission.complete_time < now_date) {
-                    deckPort[key].mission.counter = 0;
+            deckPort.deck_ports.forEach((_, key) => {
+                if (deckPort.deck_ports[key].mission.complete_time < now_date) {
+                    deckPort.deck_ports[key].mission.counter = 0;
                 } else {
-                    deckPort[key].mission.counter = deckPort[key].mission.complete_time - now_date;
+                    deckPort.deck_ports[key].mission.counter = deckPort.deck_ports[key].mission.complete_time - now_date;
                 }
             });
         }, 1000);
@@ -45,25 +45,25 @@ export const Expedition = component$<ExpeditionProps>(({ deckPort }) => {
                         Expedition
                     </summary>
                     <ul class="pl-0">
-                        { deckPort.map((_, key) => (
+                        { deckPort.deck_ports.map((_, key) => (
                             <li class="h-6">
                                 <a class="justify-start gap-0">
                                     <div class="pl-2 pr-0.5 truncate flex-1 min-w-12">
                                         <div class="w-24">
-                                            { fleet_name[deckPort[key].id] ?? "Unknown" }
+                                            { fleet_name[deckPort.deck_ports[key].id] ?? "Unknown" }
                                         </div>
                                     </div>
                                     <div class="divider divider-horizontal mr-0 ml-0 flex-none"></div>
                                     <div class="w-auto">
                                         <span class="countdown font-mono text-2xs">
-                                            <span style={{"--value":Math.floor(deckPort[key].mission.counter/3600)}}></span>:
-                                            <span style={{"--value":Math.floor(deckPort[key].mission.counter/60)%60}}></span>:
-                                            <span style={{"--value":Math.floor(deckPort[key].mission.counter)%60}}></span>
+                                            <span style={{"--value":Math.floor(deckPort.deck_ports[key].mission.counter/3600)}}></span>:
+                                            <span style={{"--value":Math.floor(deckPort.deck_ports[key].mission.counter/60)%60}}></span>:
+                                            <span style={{"--value":Math.floor(deckPort.deck_ports[key].mission.counter)%60}}></span>
                                         </span>
                                     </div>
                                     <div class="divider divider-horizontal mr-0 ml-0 flex-none"></div>
                                     <div>
-                                        {deckPort[key].mission.mission_id == 0 ? "Anchorage" : (deckPort[key].mission.counter == 0 ? "Complete" : "expedition")}
+                                        {deckPort.deck_ports[key].mission.mission_id == 0 ? "Anchorage" : (deckPort.deck_ports[key].mission.counter == 0 ? "Complete" : "expedition")}
                                     </div>
                                 </a>
                             </li>
