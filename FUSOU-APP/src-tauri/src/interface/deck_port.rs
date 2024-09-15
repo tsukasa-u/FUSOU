d15155532f79cfe4e977f4fa3472e6c7d3dcec78
Oguri Hideyuki <ogu.hide.u@gmail.com>
2024-09-15 20:05:13 +0900

    Update deck_port.rs

--- FUSOU-APP/src-tauri/src/interface/deck_port.rs
+++ FUSOU-APP/src-tauri/src/interface/deck_port.rs
@@ -1,8 +1,19 @@
+use std::collections::HashMap;
+
 use crate::kcapi;
+
+use std::sync::{LazyLock, Mutex};
+
+// Is it better to use onecell::sync::Lazy or std::sync::Lazy?
+static KC_DECKS: LazyLock<Mutex<DeckPorts>> = LazyLock::new(|| {
+    Mutex::new(DeckPorts {
+        deck_ports: HashMap::new()
+    })
+});
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct DeckPorts {
-    pub deck_ports: Vec<DeckPort>
+    pub deck_ports: HashMap<i64, DeckPort>
 }
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
@@ -12,21 +23,24 @@ pub struct DeckPort {
     pub ship: Option<Vec<i64>>,
 }
 
-
 impl From<Vec<kcapi::api_port::port::ApiDeckPort>> for DeckPorts {
     fn from(deck_ports: Vec<kcapi::api_port::port::ApiDeckPort>) -> Self {
-        let mut deck_port_list = Vec::with_capacity(4);
+        let mut deck_port_list = HashMap::with_capacity(4);
         for deck_port in deck_ports {
-            deck_port_list.push(
-                DeckPort {
-                    id: deck_port.api_id,
-                    mission: deck_port.api_mission,
-                    ship: Some(deck_port.api_ship),
-                }
-            );
+            deck_port_list.insert(deck_port.api_id, deck_port.into());
         }
         Self {
             deck_ports: deck_port_list
+        }
+    }
+}
+
+impl From<kcapi::api_port::port::ApiDeckPort> for DeckPort {
+    fn from(deck_port: kcapi::api_port::port::ApiDeckPort) -> Self {
+        Self {
+            id: deck_port.api_id,
+            mission: deck_port.api_mission,
+            ship: Some(deck_port.api_ship),
         }
     }
 }
