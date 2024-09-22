11461a2c8308bafd1168430f050a725e5ce2968a
Oguri Hideyuki <ogu.hide.u@gmail.com>
2024-09-22 16:31:14 +0900

    Update decks.tsx

--- FUSOU-APP/src/components/decks.tsx
+++ FUSOU-APP/src/components/decks.tsx
@@ -1,17 +1,21 @@
 import { Slot, component$, useStylesScoped$ } from '@builder.io/qwik';
 
 import { Deck } from './deck';
+import { DeckGrid } from './deck_grid';
 import { DeckPorts, Ships } from './interface/port';
-import { MstShips } from './interface/get_data';
+import { MstShips, MstSlotitems } from './interface/get_data';
+import { SlotItems } from './interface/require_info';
 
 interface DecksProps {
     decks: DeckPorts;
     ships: Ships;
     mst_ships: MstShips;
+    slot_items: SlotItems;
+    mst_slot_items: MstSlotitems;
 }
 
 
-export const Decks = component$<DecksProps>(({ decks, ships, mst_ships }) => {
+export const Decks = component$<DecksProps>(({ decks, ships, mst_ships, slot_items, mst_slot_items }) => {
 
     useStylesScoped$(`
         div::before, div::after {
@@ -33,20 +37,14 @@ export const Decks = component$<DecksProps>(({ decks, ships, mst_ships }) => {
                                 <Slot name={`icon_fleet${idx}`} />
                             </Deck>
                         ))} */}
-                        {/* { Object.entries(decks.deck_ports).map(([key, deck]) => (
-                            // <>{deck.ship}</>
-                            // <>{decks.deck_ports[Number(key)].ship}</>
+                        { Object.entries(decks.deck_ports).map(([key, deck]) => (
                             <>
-                                <>{deck.ship}</>
-                                <Deck deckPort={ deck } ships={ ships } mst_ships={ mst_ships }>
+                                <Deck deckPort={ deck } ships={ ships } mst_ships={ mst_ships } slot_items={ slot_items } mst_slot_items={ mst_slot_items }>
                                     <Slot name={`icon_fleet${key}`} />
                                 </Deck>
                             </>
-                            // <Deck deckPorts={ decks } ships={ ships } mst_ships={ mst_ships } idx={Number(key)}>
-                            //     <Slot name={`icon_fleet${key}`} />
-                            // </Deck>
-                        )) } */}
-                        <Deck deckPort={ decks.deck_ports[1] } ships={ ships } mst_ships={ mst_ships }>
+                        )) }
+                        {/* <Deck deckPort={ decks.deck_ports[1] } ships={ ships } mst_ships={ mst_ships }>
                             <Slot name="icon_fleet1" />
                         </Deck>
                         <Deck deckPort={ decks.deck_ports[2] } ships={ ships } mst_ships={ mst_ships }>
@@ -57,7 +55,7 @@ export const Decks = component$<DecksProps>(({ decks, ships, mst_ships }) => {
                         </Deck>
                         <Deck deckPort={ decks.deck_ports[4]} ships={ ships } mst_ships={ mst_ships }>
                             <Slot name="icon_fleet4" />
-                        </Deck>
+                        </Deck> */}
                     </ul>
                 </details>
             </li>
