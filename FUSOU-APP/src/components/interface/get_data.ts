dc68d8243942b42e39859ddc355acb82b0677695
Oguri Hideyuki <ogu.hide.u@gmail.com>
2024-09-22 16:32:17 +0900

    Update get_data.ts

--- FUSOU-APP/src/components/interface/get_data.ts
+++ FUSOU-APP/src/components/interface/get_data.ts
@@ -43,3 +43,46 @@ export var global_mst_ships: MstShips = {
         3: { id: 3, sortno: 3, sort_id: 3, name: "重巡洋艦", yomi: "じゅうじゅんようかん", stype: 5, ctype: 5, afterlv: 40, aftershipid: "4", taik: [30, 30], souk: [15, 15], houg: [15, 15], raig: [15, 15], tyku: [15, 15], luck: [30, 30], soku: 10, leng: 3, slot_num: 4, maxeq: [3, 3], buildtime: 40, broken: [0, 0], powup: [0, 0], backs: 1, getmes: "入手メッセージ", afterfuel: 40, afterbull: 40, fuel_max: 100, bull_max: 100, voicef: 0, tais: [0, 0] }
     }
 };
+
+export interface MstSlotitem {
+    id: number,
+    sortno: number,
+    name: string,
+    _type: number[], // length: 5
+    taik: number,
+    souk: number,
+    houg: number,
+    raig: number,
+    soku: number,
+    baku: number,
+    tyku: number,
+    tais: number,
+    atap: number,
+    houm: number,
+    raim: number,
+    houk: number,
+    raik: number,
+    bakk: number,
+    saku: number,
+    sakb: number,
+    luck: number,
+    leng: number,
+    rare: number,
+    broken: number[],
+    usebull: string,
+    version?: number,
+    cost?: number,
+    distance?: number,
+}
+
+export interface MstSlotitems {
+    mst_slot_items: { [key: number]: MstSlotitem }
+}
+
+export var global_mst_slot_items: MstSlotitems = {
+    mst_slot_items:{
+        0: { id: 0, sortno: 0, name: "なし", _type: [0, 0, 0, 0, 0], taik: 0, souk: 0, houg: 0, raig: 0, soku: 0, baku: 0, tyku: 0, tais: 0, atap: 0, houm: 0, raim: 0, houk: 0, raik: 0, bakk: 0, saku: 0, sakb: 0, luck: 0, leng: 0, rare: 0, broken: [0, 0], usebull: "なし" },
+        1: { id: 1, sortno: 1, name: "12.7cm連装砲", _type: [1, 1, 1, 0, 0], taik: 0, souk: 0, houg: 2, raig: 0, soku: 0, baku: 0, tyku: 1, tais: 0, atap: 0, houm: 0, raim: 0, houk: 1, raik: 0, bakk: 0, saku: 0, sakb: 0, luck: 0, leng: 1, rare: 0, broken: [0, 0], usebull: "なし" },
+        2: { id: 2, sortno: 2, name: "12.7cm連装砲B型", _type: [1, 1, 1, 0, 0], taik: 0, souk: 0, houg: 2, raig: 0, soku: 0, baku: 0, tyku: 1, tais: 0, atap: 0, houm: 0, raim: 0, houk: 1, raik: 0, bakk: 0, saku: 0, sakb: 0, luck: 0, leng: 1, rare: 0, broken: [0, 0], usebull: "なし" },
+    }
+};
