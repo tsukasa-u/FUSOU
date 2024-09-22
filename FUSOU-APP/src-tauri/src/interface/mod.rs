0f1ee6bfd637681fdfc98c2a5096db8c2bae4e59
Oguri Hideyuki <ogu.hide.u@gmail.com>
2024-09-22 16:29:54 +0900

    Update mod.rs

--- FUSOU-APP/src-tauri/src/interface/mod.rs
+++ FUSOU-APP/src-tauri/src/interface/mod.rs
@@ -1,9 +1,11 @@
 pub mod material;
 pub mod ship;
+pub mod slot_item;
 pub mod logs;
 pub mod deck_port;
 pub mod mission;
 pub mod n_dock;
 pub mod mst_ship;
+pub mod mst_slot_item;
 
 pub mod interface;
