3add71f4e16ea3fc979b99c43ec0b34f34ca5d56
Oguri Hideyuki <ogu.hide.u@gmail.com>
2024-09-26 19:47:16 +0900

    Update settings.tsx

--- FUSOU-APP/src/components/settings.tsx
+++ FUSOU-APP/src/components/settings.tsx
@@ -1,6 +1,7 @@
 import { component$, useStylesScoped$ } from '@builder.io/qwik';
 
 import { invoke } from '@tauri-apps/api/tauri'
+import { FadeToast, showFadeToast } from './fade_toast';
 
 export const Settings = component$(() => {
 
@@ -35,27 +36,28 @@ export const Settings = component$(() => {
                         <h2 class="text-lg font-semibold leading-4 text-slate-700">Load ship data</h2>
                         <p class="text-slate-600">Load the mst_ships data restored from API "/kcsapi/api_start2/get_data"</p>
                         <div class="mt-4 flex items-center justify-end">
-                            <button class="btn btn-outline btn-wide" onClick$={() => { invoke("get_mst_ships") }}>Load mst ship data</button>
+                            <button class="btn btn-outline btn-wide" onClick$={() => { invoke("get_mst_ships"); showFadeToast('setting_toast', "load mst_ships");  }}>Load mst ship data</button>
                         </div>
                     </div>
                     <div id="load_slot_items" class="py-2">
                         <h2 class="text-lg font-semibold leading-4 text-slate-700">Load slotitems data</h2>
                         <p class="text-slate-600">Load the slotitems data restored from API  "/kcsapi/api_get_member/require_info"</p>
                         <div class="mt-4 flex items-center justify-end">
-                            <button class="btn btn-outline btn-wide" onClick$={() => { invoke("get_slot_items") }}>Load slot item data</button>
+                            <button class="btn btn-outline btn-wide" onClick$={() => { invoke("get_slot_items"); showFadeToast('setting_toast', "load slot_items");  }}>Load slot item data</button>
                         </div>
                     </div> 
                     <div id="load_mst_slot_items" class="py-2">
                         <h2 class="text-lg font-semibold leading-4 text-slate-700">Load mst_slotitems data</h2>
                         <p class="text-slate-600">Load the mst_slotitems data restored from API "/kcsapi/api_start2/get_data"</p>
                         <div class="mt-4 flex items-center justify-end">
-                            <button class="btn btn-outline btn-wide" onClick$={() => { invoke("get_mst_slot_items") }}>Load mst slot item data</button>
+                            <button class="btn btn-outline btn-wide" onClick$={() => { invoke("get_mst_slot_items"); showFadeToast('setting_toast', "load mst_slot_items"); }}>Load mst slot item data</button>
                         </div>
                     </div> 
                 </div>
 
                 <hr class="mt-4 mb-8" />
             </div>
+            <FadeToast toast_id='setting_toast'></FadeToast>
         </>
     );
 });
