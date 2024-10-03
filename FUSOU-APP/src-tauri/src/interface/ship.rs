a3b626cd8a70d6e897be5b589970041a84149a9c
Oguri Hideyuki <ogu.hide.u@gmail.com>
2024-10-03 23:23:50 +0900

    Update ship.rs

--- FUSOU-APP/src-tauri/src/interface/ship.rs
+++ FUSOU-APP/src-tauri/src/interface/ship.rs
@@ -1,6 +1,18 @@
 use std::collections::HashMap;
 
+use serde::de;
+
 use crate::kcapi;
+use crate::interface::deck_port::KCS_DECKS;
+
+use std::sync::{LazyLock, Mutex};
+
+// Is it better to use onecell::sync::Lazy or std::sync::Lazy?
+pub static KCS_SHIPS: LazyLock<Mutex<Ships>> = LazyLock::new(|| {
+    Mutex::new(Ships {
+        ships: HashMap::new()
+    })
+});
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct Ships {
@@ -10,28 +22,28 @@ pub struct Ships {
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct Ship {
     pub id:         i64,
-    pub ship_id:    i64,
-    pub lv:         i64,      // レベル
-    pub exp:        Vec<i64>,    
+    pub ship_id:    Option<i64>,
+    pub lv:         Option<i64>,      // レベル
+    pub exp:        Option<Vec<i64>>,    
     pub nowhp:      i64,      // 現在HP
     pub maxhp:      i64,      // 最大HP
-    pub soku:       i64,      // 速力
-    pub leng:       i64,      // 射程
-    pub slot:       Vec<i64>,    // 装備
-    pub onsolot:    Vec<i64>,    // 艦載機搭載数
-    pub slot_ex:    i64,      // 補強増設
-    pub fuel:       i64,      // 燃料
-    pub bull:       i64,      // 弾薬
-    pub slotnum:    i64,      // 装備スロット数
-    pub cond:       i64,      // 疲労度
-    pub karyoku:    Vec<i64>,      // 火力
-    pub raisou:     Vec<i64>,      // 雷装
-    pub taiku:      Vec<i64>,      // 対空
-    pub soukou:     Vec<i64>,      // 装甲
-    pub kaihi:      Vec<i64>,      // 回避
-    pub taisen:     Vec<i64>,      // 対潜
-    pub sakuteki:   Vec<i64>,      // 索敵
-    pub lucky:      Vec<i64>,      // 運
+    pub soku:       Option<i64>,      // 速力
+    pub leng:       Option<i64>,      // 射程
+    pub slot:       Option<Vec<i64>>,    // 装備
+    pub onsolot:    Option<Vec<i64>>,    // 艦載機搭載数
+    pub slot_ex:    Option<i64>,      // 補強増設
+    pub fuel:       Option<i64>,      // 燃料
+    pub bull:       Option<i64>,      // 弾薬
+    pub slotnum:    Option<i64>,      // 装備スロット数
+    pub cond:       Option<i64>,      // 疲労度
+    pub karyoku:    Option<Vec<i64>>,      // 火力
+    pub raisou:     Option<Vec<i64>>,      // 雷装
+    pub taiku:      Option<Vec<i64>>,      // 対空
+    pub soukou:     Option<Vec<i64>>,      // 装甲
+    pub kaihi:      Option<Vec<i64>>,      // 回避
+    pub taisen:     Option<Vec<i64>>,      // 対潜
+    pub sakuteki:   Option<Vec<i64>>,      // 索敵
+    pub lucky:      Option<Vec<i64>>,      // 運
     pub sally_area: Option<i64>,
     pub sp_effect_items: Option<SpEffectItems>,
 }
@@ -67,28 +79,28 @@ impl From<kcapi::api_port::port::ApiShip> for Ship {
     fn from(ship: kcapi::api_port::port::ApiShip) -> Self {
         Self {
             id:         ship.api_id,
-            ship_id:    ship.api_ship_id,
-            lv:         ship.api_lv,
-            exp:        ship.api_exp,
+            ship_id:    Some(ship.api_ship_id),
+            lv:         Some(ship.api_lv),
+            exp:        Some(ship.api_exp),
             nowhp:      ship.api_nowhp,
             maxhp:      ship.api_maxhp,
-            soku:       ship.api_soku,
-            leng:       ship.api_leng,
-            slot:       ship.api_slot,
-            onsolot:    ship.api_onslot,
-            slot_ex:    ship.api_slot_ex,
-            fuel:       ship.api_fuel,
-            bull:       ship.api_bull,
-            slotnum:    ship.api_slotnum,
-            cond:       ship.api_cond,
-            karyoku:    ship.api_karyoku,
-            raisou:     ship.api_raisou,
-            taiku:      ship.api_taiku,
-            soukou:     ship.api_soukou,
-            kaihi:      ship.api_kaihi,
-            taisen:     ship.api_taisen,
-            sakuteki:   ship.api_sakuteki,
-            lucky:      ship.api_lucky,
+            soku:       Some(ship.api_soku),
+            leng:       Some(ship.api_leng),
+            slot:       Some(ship.api_slot),
+            onsolot:    Some(ship.api_onslot),
+            slot_ex:    Some(ship.api_slot_ex),
+            fuel:       Some(ship.api_fuel),
+            bull:       Some(ship.api_bull),
+            slotnum:    Some(ship.api_slotnum),
+            cond:       Some(ship.api_cond),
+            karyoku:    Some(ship.api_karyoku),
+            raisou:     Some(ship.api_raisou),
+            taiku:      Some(ship.api_taiku),
+            soukou:     Some(ship.api_soukou),
+            kaihi:      Some(ship.api_kaihi),
+            taisen:     Some(ship.api_taisen),
+            sakuteki:   Some(ship.api_sakuteki),
+            lucky:      Some(ship.api_lucky),
             sally_area: ship.api_sally_area,
             sp_effect_items: match ship.api_sp_effect_items {
                 Some(items) => Some(items.into()),
@@ -122,33 +134,52 @@ impl From<kcapi::api_port::port::ApiSpEffectItem> for SpEffectItem {
     }
 }
 
-// impl From<kcapi::api_req_sortie::battle::ApiData> for Ships {
-//     fn from(ship: kcapi::api_req_sortie::battle::ApiData) -> Self {
-//         Self {
-//             id:         ship.api_id,
-//             ship_id:    ship.api_ship_id,
-//             lv:         ship.api_lv,
-//             exp:        ship.api_exp,
-//             nowhp:      ship.api_nowhp,
-//             maxhp:      ship.api_maxhp,
-//             soku:       ship.api_soku,
-//             leng:       ship.api_leng,
-//             slot:       ship.api_slot,
-//             onsolot:    ship.api_onslot,
-//             slot_ex:    ship.api_slot_ex,
-//             fuel:       ship.api_fuel,
-//             bull:       ship.api_bull,
-//             slotnum:    ship.api_slotnum,
-//             cond:       ship.api_cond,
-//             karyoku:    ship.api_karyoku,
-//             raisou:     ship.api_raisou,
-//             taiku:      ship.api_taiku,
-//             soukou:     ship.api_soukou,
-//             kaihi:      ship.api_kaihi,
-//             taisen:     ship.api_taisen,
-//             sakuteki:   ship.api_sakuteki,
-//             lucky:      ship.api_lucky,
-//             sally_area: ship.api_sally_area,
-//         }
-//     }
-// }
+impl From<kcapi::api_req_sortie::battle::ApiData> for Ships {
+    fn from(battle_data: kcapi::api_req_sortie::battle::ApiData) -> Self {
+        let mut ship_map = HashMap::<i64, Ship>::with_capacity(7);
+
+        let deck_id = battle_data.api_deck_id.clone();
+        let deck_ports = KCS_DECKS.lock().unwrap();
+
+        if let Some(deck) = (*deck_ports).deck_ports.get(&deck_id).clone() {
+            if let Some(ship_ids) = &deck.ship {
+                for (idx, id) in ship_ids.iter().enumerate() {
+                    ship_map.insert(
+                        id.clone(), 
+                        Ship {
+                            id:         id.clone(),
+                            ship_id:    None,
+                            lv:         None,
+                            exp:        None,
+                            nowhp:      battle_data.api_f_nowhps[idx],
+                            maxhp:      battle_data.api_f_maxhps[idx],
+                            soku:       None,
+                            leng:       None,
+                            slot:       None,
+                            onsolot:    None,
+                            slot_ex:    None,
+                            fuel:       None,
+                            bull:       None,
+                            slotnum:    None,
+                            cond:       None,
+                            karyoku:    None,
+                            raisou:     None,
+                            taiku:      None,
+                            soukou:     None,
+                            kaihi:      None,
+                            taisen:     None,
+                            sakuteki:   None,
+                            lucky:      None,
+                            sally_area: None,
+                            sp_effect_items: None,
+                        }
+                    );
+                }
+            }
+        }
+
+        Ships {
+            ships: ship_map
+        }
+    }
+}
