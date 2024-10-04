e236571f4754016c170612d9d46b4b444d3230bc
Oguri Hideyuki <ogu.hide.u@gmail.com>
2024-10-05 01:45:30 +0900

    update

---
+++ FUSOU-APP/src/components/fuel_bullet_color_bar.tsx
@@ -0,0 +1,30 @@
+import { component$, JSXOutput, useComputed$, PropsOf } from '@builder.io/qwik';
+
+interface ColorBarProps {
+    v_now: number;
+    v_max: number;
+}
+
+export const FuelBulletColorBar = component$(({v_now, v_max, ...props}: ColorBarProps & PropsOf<'progress'>) => {
+    const progress_color_state = useComputed$(() => {
+        let props_expand = { ...props, value: v_max != 0 ? (v_now ?? 0) * 100 / (v_max ?? -1) : 0, max: 100 };
+        const progress_color_list: JSXOutput[] = [
+            <progress {...props_expand} class={[props.class, "progress [&::-webkit-progress-value]:bg-black      [&::-moz-progress-bar]:bg-black"]}></progress>,
+            <progress {...props_expand} class={[props.class, "progress [&::-webkit-progress-value]:bg-red-500    [&::-moz-progress-bar]:bg-red-500"]}></progress>,
+            <progress {...props_expand} class={[props.class, "progress [&::-webkit-progress-value]:bg-orange-500 [&::-moz-progress-bar]:bg-orange-500"]}></progress>,
+            <progress {...props_expand} class={[props.class, "progress [&::-webkit-progress-value]:bg-yellow-500 [&::-moz-progress-bar]:bg-yellow-500"]}></progress>,
+            <progress {...props_expand} class={[props.class, "progress [&::-webkit-progress-value]:bg-green-500  [&::-moz-progress-bar]:bg-green-500"]}></progress>,
+        ];
+
+        let color: JSXOutput = <></>;
+        if (v_now == v_max) color = progress_color_list[4];
+        else if (v_now > 7/9*v_max) color = progress_color_list[3];
+        else if (v_now > 3/9*v_max) color = progress_color_list[2];
+        else if (v_now >= 0) color = progress_color_list[1];
+        return color;
+    });
+
+    return <>
+        { progress_color_state }
+    </>;
+});
