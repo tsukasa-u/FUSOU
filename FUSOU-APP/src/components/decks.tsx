46ef67eab95a960d70b878176dcc7cd8d01365fc
Oguri Hideyuki <ogu.hide.u@gmail.com>
2024-10-14 14:20:28 +0900

    update

--- FUSOU-APP/src/components/decks.tsx
+++ FUSOU-APP/src/components/decks.tsx
@@ -6,19 +6,25 @@ import "../css/divider.css";
 
 export function DecksComponent() {
 
+    const fleet_name: {[key:number]: string} = {
+        1: "First Fleet",
+        2: "Second Fleet",
+        3: "Third Fleet",
+        4: "Fourth Fleet",
+    }
 
     const [decks, ] =  useDeckPorts();
 
     return (
         <>
             <li>
-                <details open>
+                <details open={true}>
                     <summary>
                         Fleets
                     </summary>
                     <ul class="pl-0">
                         <For each={Object.entries(decks.deck_ports)} fallback={<div class="text-xs py-2">Loading Fleet Data ...</div>}>
-                            {(item) => <>{<DeckComponent deck_id={Number(item[0])}></DeckComponent>}</>}
+                            {(item) => <>{<DeckComponent deck_id={Number(item[0])} fleet_name={fleet_name[Number(item[0])]}></DeckComponent>}</>}
                         </For>
                     </ul>
                 </details>
