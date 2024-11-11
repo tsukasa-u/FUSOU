52fb8d19aa28ec26df3fe07a43cf00d84cee287e
Oguri Hideyuki <ogu.hide.u@gmail.com>
2024-11-11 22:04:05 +0900

    add router

--- FUSOU-APP/src/main.tsx
+++ FUSOU-APP/src/main.tsx
@@ -1,8 +1,28 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
+import { Router, Route } from "@solidjs/router";
+
 import App from './pages/app.tsx'
+import Start from './pages/start.tsx'
+import NotFound from "./pages/not_found.tsx";
 
-// import './index.css'
 import './tailwind.css'
+import { JSX } from "solid-js";
 
-render(() => <App />, document.getElementById("root") as HTMLElement);
+const Root = (props: {children: JSX.Element}) => (
+    <>
+      {props.children}
+    </>
+);
+
+render(
+    () => (
+        <Router root={Root}>
+            <Route path="/app" component={App} />
+            <Route path="/" component={Start} />
+            <Route path="*paramName" component={NotFound} />
+        </Router>
+    ),
+    document.getElementById("root") as HTMLElement
+);
+// render(() => <App />, document.getElementById("root") as HTMLElement);
