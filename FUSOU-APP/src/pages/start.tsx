c60c39596e4e5432c2e004cb03b78fb6bf45d897
Oguri Hideyuki <ogu.hide.u@gmail.com>
2024-11-11 22:04:05 +0900

    add router

---
+++ FUSOU-APP/src/pages/start.tsx
@@ -0,0 +1,11 @@
+function Start() {
+  
+    return (
+      <>
+        <h1>Start</h1>
+        <a role="button" class="btn" href="/app">Link</a>
+      </>
+    );
+  }
+  
+  export default Start;
