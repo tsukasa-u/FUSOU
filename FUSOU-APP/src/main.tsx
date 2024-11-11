b0c814ffe50d5d45b012ffbdc1310e9a18810c77
Oguri Hideyuki <ogu.hide.u@gmail.com>
2024-11-11 21:04:52 +0900

    resolve path

--- FUSOU-APP/src/main.tsx
+++ FUSOU-APP/src/main.tsx
@@ -1,8 +1,8 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-import App from './app.tsx'
+import App from './pages/app.tsx'
 
-import './index.css'
+// import './index.css'
 import './tailwind.css'
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
