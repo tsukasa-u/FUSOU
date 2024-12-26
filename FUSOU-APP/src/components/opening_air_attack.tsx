8eca203e754261754d70876391cbbd440d65dbe1
Oguri Hideyuki <ogu.hide.u@gmail.com>
2024-12-26 22:23:06 +0900

    Update opening_air_attack.tsx

--- FUSOU-APP/src/components/opening_air_attack.tsx
+++ FUSOU-APP/src/components/opening_air_attack.tsx
@@ -41,7 +41,6 @@ export function OpeningAirAttackComponent({deck_ship_id, battle_selected}: AirDa
         if (battle_selected().opening_air_attack == null) return false;
         if (battle_selected().opening_air_attack.f_damage == null) return false;
         if (battle_selected().opening_air_attack.e_damage == null) return false;
-        console.log(battle_selected().opening_air_attack);
         return true;
     });
 
