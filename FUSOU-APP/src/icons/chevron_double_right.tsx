dc5af57a23001ee5be10331861731ea8e7eedbef
Oguri Hideyuki <ogu.hide.u@gmail.com>
2025-01-09 12:07:14 +0900

    add svg

---
+++ FUSOU-APP/src/icons/chevron_double_right.tsx
@@ -0,0 +1,15 @@
+import { JSX } from 'solid-js';
+ 
+export function IconChevronDoubleRight(props: JSX.HTMLAttributes<SVGSVGElement>) {
+  return (
+    <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+      stroke-width="0" 
+      stroke="currentColor" 
+	    viewBox="0 0 16 16"
+      {...props}>
+      	<path fill-rule="evenodd" d="M3.646 1.646a0.5 0.5 0 0 1 0.708 0l6 6a0.5 0.5 0 0 1 0 0.708l-6 6a0.5 0.5 0 0 1 -0.708 -0.708L9.293 8 3.646 2.354a0.5 0.5 0 0 1 0 -0.708"></path>
+        <path fill-rule="evenodd" d="M7.646 1.646a0.5 0.5 0 0 1 0.708 0l6 6a0.5 0.5 0 0 1 0 0.708l-6 6a0.5 0.5 0 0 1 -0.708 -0.708L13.293 8 7.646 2.354a0.5 0.5 0 0 1 0 -0.708"></path>
+    </svg>
+  )
+}
+export default IconChevronDoubleRight
---
+++ FUSOU-APP/src/icons/chevron_right_s.tsx
@@ -0,0 +1,14 @@
+import { JSX } from 'solid-js';
+ 
+export function IconChevronRightS(props: JSX.HTMLAttributes<SVGSVGElement>) {
+  return (
+    <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+      stroke-width="1.5" 
+      stroke="currentColor" 
+	 viewBox="0 0 21 21"
+      {...props}>
+        <path fill="none" d="m 10 16 l 5 -5 l -5 -5"/>
+    </svg>
+  )
+}
+export default IconChevronRightS
