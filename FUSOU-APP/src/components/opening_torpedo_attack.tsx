18700733269a4163d5650aa8464787029d687d35
Oguri Hideyuki <ogu.hide.u@gmail.com>
2025-01-13 13:43:48 +0900

    Update opening_torpedo_attack.tsx

--- FUSOU-APP/src/components/opening_torpedo_attack.tsx
+++ FUSOU-APP/src/components/opening_torpedo_attack.tsx
@@ -114,7 +114,7 @@ export function OpeningTorpedoAttackComponent({deck_ship_id, battle_selected}: T
                             <tbody>
                                 <For each={opening_torpedo_damage().frai.list}>
                                     {(frai, _) => (
-                                        <tr>
+                                        <tr class="table_hover table_active rounded">
                                             <td>
                                                 <div class="flex flex-col">
                                                     <For each={opening_torpedo_damage().frai.dict[frai].ships}>
@@ -130,7 +130,7 @@ export function OpeningTorpedoAttackComponent({deck_ship_id, battle_selected}: T
                                                 </div>
                                             </td>
                                             <td>
-                                                <EnemyNameComponent ship_id={battle_selected().enemy_ship_id[frai]}></EnemyNameComponent>
+                                                <EnemyNameComponent ship_id={battle_selected().enemy_ship_id[frai]} ship_max_hp={battle_selected().e_hp_max![frai]} ship_param={battle_selected().e_params![frai]} ship_slot={battle_selected().e_slot![frai]}></EnemyNameComponent>
                                             </td>
                                             <td >
                                                 <div class={
@@ -149,12 +149,12 @@ export function OpeningTorpedoAttackComponent({deck_ship_id, battle_selected}: T
                                 </For>
                                 <For each={opening_torpedo_damage().erai.list}>
                                     {(erai, _) => (
-                                        <tr>
+                                        <tr class="table_hover table_active rounded">
                                             <td>
                                                 <div class="flex flex-col">
                                                     <For each={opening_torpedo_damage().erai.dict[erai].ships}>
                                                         {(ship_id, _) => (
-                                                            <EnemyNameComponent ship_id={battle_selected().enemy_ship_id[ship_id]}></EnemyNameComponent>
+                                                            <EnemyNameComponent ship_id={battle_selected().enemy_ship_id[ship_id]} ship_max_hp={battle_selected().e_hp_max![ship_id]} ship_param={battle_selected().e_params![ship_id]} ship_slot={battle_selected().e_slot![ship_id]}></EnemyNameComponent>
                                                         )}
                                                     </For>
                                                 </div>
