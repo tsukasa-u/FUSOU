8f04533dd8f9e11606aaf3873f7f25f01b97b013
Oguri Hideyuki <ogu.hide.u@gmail.com>
2025-02-17 21:00:38 +0900

    add Debug & Release build option

--- FUSOU-APP/src-tauri/build.rs
+++ FUSOU-APP/src-tauri/build.rs
@@ -1,11 +1,19 @@
 fn main() {
   // println!("cargo:rustc-cfg=TAURI_BUILD_DEBUG");
-  
-  if std::env::var("TAURI_BUILD_DEBUG").is_ok() {
-    println!("cargo:rustc-cfg=TAURI_BUILD_DEBUG");
-    println!("cargo::rustc-check-cfg=cfg(TAURI_BUILD_DEBUG)");
+  println!("cargo:rustc-check-cfg=cfg(TAURI_BUILD_TYPE, values(\"DEBUG\"))");
+  println!("cargo:rustc-check-cfg=cfg(TAURI_BUILD_TYPE, values(\"RELEASE\"))");
+  match std::env::var("TAURI_BUILD_TYPE") {
+    Ok(val) => {
+      if val == "DEBUG" {
+        println!("cargo:rustc-cfg=TAURI_BUILD_TYPE=\"DEBUG\"");
+      } else if val == "RELEASE" {
+        println!("cargo:rustc-cfg=TAURI_BUILD_TYPE=\"RELEASE\"");
+      }
+    },
+    Err(_) => {
+      println!("cargo:rustc-cfg=TAURI_BUILD_TYPE=\"DEBUG\"");
+    }
   }
-  println!("cargo:warning={:?}", std::env::var("TAURI_BUILD_DEBUG"));
 
   tauri_build::build();
   println!("cargo:rustc-env=RUST_TEST_NOCAPTURE=1");
