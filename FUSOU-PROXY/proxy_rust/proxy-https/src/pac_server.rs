d01bccccf52833e2e4a9a9994762229c02a3520a
Oguri Hideyuki <ogu.hide.u@gmail.com>
2025-03-21 16:48:50 +0900

    autofix with clippy

--- FUSOU-PROXY/proxy_rust/proxy-https/src/pac_server.rs
+++ FUSOU-PROXY/proxy_rust/proxy-https/src/pac_server.rs
@@ -4,13 +4,13 @@ use warp::Filter;
 
 use crate::bidirectional_channel;
 
-pub static PATH_PROXY_CRATE: &'static str = env!("CARGO_MANIFEST_DIR");
-pub static PATH_ADD_PROXY_BAT: &'static str = "cmd/add_proxy.bat";
-pub static PATH_DELETE_PROXY_BAT: &'static str = "cmd/delete_proxy.bat";
-pub static PATH_ADD_STORE_BAT: &'static str = "cmd/add_store.bat";
-pub static PATH_ADD_PROXY_SH: &'static str = "cmd/add_proxy.sh";
-pub static PATH_DELETE_PROXY_SH: &'static str = "cmd/delete_proxy.sh";
-pub static PATH_ADD_STORE_SH: &'static str = "cmd/add_store.sh";
+pub static PATH_PROXY_CRATE: &str = env!("CARGO_MANIFEST_DIR");
+pub static PATH_ADD_PROXY_BAT: &str = "cmd/add_proxy.bat";
+pub static PATH_DELETE_PROXY_BAT: &str = "cmd/delete_proxy.bat";
+pub static PATH_ADD_STORE_BAT: &str = "cmd/add_store.bat";
+pub static PATH_ADD_PROXY_SH: &str = "cmd/add_proxy.sh";
+pub static PATH_DELETE_PROXY_SH: &str = "cmd/delete_proxy.sh";
+pub static PATH_ADD_STORE_SH: &str = "cmd/add_store.sh";
 
 pub fn serve_pac_file(
     path: String,
