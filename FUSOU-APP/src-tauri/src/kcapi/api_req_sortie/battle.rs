e6f5d82bbb2fda28b8c0e2c0a6beb2debc2e6e65
Oguri Hideyuki <ogu.hide.u@gmail.com>
2025-03-28 00:10:30 +0900

    update

--- FUSOU-APP/src-tauri/src/kcapi/api_req_sortie/battle.rs
+++ FUSOU-APP/src-tauri/src/kcapi/api_req_sortie/battle.rs
@@ -35,7 +35,7 @@ use crate::kcapi_common::common_battle::ApiSupportInfo;
 #[serde(rename_all = "camelCase")]
 pub struct Req {}
 
-#[derive(Getter, TraitForTest, TraitForRoot, )]
+#[derive(Getter, TraitForTest, TraitForRoot)]
 #[struct_test_case(field_extra, type_value, integration)]
 #[add_field(extra)]
 #[register_struct(name = "api_req_sortie/battle")]
@@ -159,10 +159,18 @@ mod tests {
 
         let pattern_str = "S@api_req_sortie@battle";
         let log_path = "./src/kcapi/api_req_sortie/battle@S.log";
-        simple_root_test::<Res>(target_path.clone(), pattern_str.to_string(), log_path.to_string());
+        simple_root_test::<Res>(
+            target_path.clone(),
+            pattern_str.to_string(),
+            log_path.to_string(),
+        );
 
         let pattern_str = "Q@api_req_sortie@battle";
         let log_path = "./src/kcapi/api_req_sortie/battle@Q.log";
-        simple_root_test::<Req>(target_path.clone(), pattern_str.to_string(), log_path.to_string());
+        simple_root_test::<Req>(
+            target_path.clone(),
+            pattern_str.to_string(),
+            log_path.to_string(),
+        );
     }
 }
