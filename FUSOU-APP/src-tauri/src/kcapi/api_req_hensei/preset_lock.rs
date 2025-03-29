cbb6977b0e536e39f9359db6c6bfd27ec673296c
Oguri Hideyuki <ogu.hide.u@gmail.com>
2025-03-30 00:53:31 +0900

    update

--- FUSOU-APP/src-tauri/src/kcapi/api_req_hensei/preset_lock.rs
+++ FUSOU-APP/src-tauri/src/kcapi/api_req_hensei/preset_lock.rs
@@ -30,6 +30,8 @@ pub struct Req {
     pub api_token: String,
     #[serde(rename = "api_verno")]
     pub api_verno: String,
+    #[serde(rename = "api_preset_no")]
+    pub api_preset_no: String,
 }
 
 #[derive(Getter, TraitForTest, TraitForRoot, TraitForConvert)]
@@ -67,10 +69,18 @@ mod tests {
 
         let pattern_str = "S@api_req_hensei@preset_lock";
         let log_path = "./src/kcapi/api_req_hensei/preset_lock@S.log";
-        simple_root_test::<Res>(target_path.clone(), pattern_str.to_string(), log_path.to_string());
+        simple_root_test::<Res>(
+            target_path.clone(),
+            pattern_str.to_string(),
+            log_path.to_string(),
+        );
 
         let pattern_str = "Q@api_req_hensei@preset_lock";
         let log_path = "./src/kcapi/api_req_hensei/preset_lock@Q.log";
-        simple_root_test::<Req>(target_path.clone(), pattern_str.to_string(), log_path.to_string());
+        simple_root_test::<Req>(
+            target_path.clone(),
+            pattern_str.to_string(),
+            log_path.to_string(),
+        );
     }
 }
