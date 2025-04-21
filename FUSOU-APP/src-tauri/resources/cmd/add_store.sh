fc7dcda6248e3328501126b1048d21247ff79eb9
Oguri Hideyuki <ogu.hide.u@gmail.com>
2025-04-22 06:44:52 +0900

    add permission for execute

--- FUSOU-APP/src-tauri/resources/cmd/add_store.sh
+++ FUSOU-APP/src-tauri/resources/cmd/add_store.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo apt-get install -y ca-certificates
-sudo cp $1 /usr/local/share/ca-certificates
-sudo update-ca-certificates
+gnome-terminal -- bash -c "echo '> sudo apt install -y ca-certificates && cp $1 /usr/local/share/ca-certificates && update-ca-certificates' && sudo apt install -y ca-certificates && cp $1 /usr/local/share/ca-certificates && update-ca-certificates"
+
+# sudo apt-get install -y ca-certificates
+# sudo cp $1 /usr/local/share/ca-certificates
+# sudo update-ca-certificates
 
 # replace $1 with your certificate file path (ca_cert.pem)
