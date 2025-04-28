3c6f1e7bf48411fca4fc487d37a9249cb8148f76
Oguri Hideyuki <ogu.hide.u@gmail.com>
2025-04-29 08:11:44 +0900

    change url

--- FUSOU-WEB/src/env.d.ts
+++ FUSOU-WEB/src/env.d.ts
@@ -1,6 +1,7 @@
 interface ImportMetaEnv {
     readonly PUBLIC_SUPABASE_URL: string
     readonly PUBLIC_SUPABASE_ANON_KEY: string
+    readonly PUBLIC_SITE_URL: string
   }
   
 interface ImportMeta {
