3ccb7918385ae5a45ce25df5a198a5ffec99a66b
Oguri Hideyuki <ogu.hide.u@gmail.com>
2025-04-29 07:25:13 +0900

    update

--- FUSOU-WEB/src/pages/api/auth/signin.ts
+++ FUSOU-WEB/src/pages/api/auth/signin.ts
@@ -4,46 +4,65 @@ import type { Provider } from "@supabase/supabase-js";
 
 export const POST: APIRoute = async ({ request, cookies, redirect }) => {
   const formData = await request.formData();
-  const email = formData.get("email")?.toString();
-  const password = formData.get("password")?.toString();
+  const url_origin = request.url.match(/http[s]?:\/\/[^:]+:[0-9]+/g)![0]
+  
+  // const email = formData.get("email")?.toString();
+  // const password = formData.get("password")?.toString();
   const provider = formData.get("provider")?.toString();
 
-  const validProviders = ["google", "github", "discord"];
+  const validProviders = ["google"];
 
   if (provider && validProviders.includes(provider)) {
-    const { data, error } = await supabase.auth.signInWithOAuth({
-      provider: provider as Provider,
-      options: {
-        redirectTo: "http://localhost:4321/api/auth/callback"
-      },
-    });
+    if (provider == "google") {
 
-    if (error) {
-      return new Response(error.message, { status: 500 });
+      const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: 'google',
+        options: {
+            scopes: 'https://www.googleapis.com/auth/drive.file',
+            redirectTo: `${url_origin}/api/auth/callback`,
+        },
+        });
+
+        if (error) {
+          return new Response(error.message, { status: 500 });
+        }
+
+        return redirect(data.url);
+    } else {
+      const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: provider as Provider,
+        options: {
+          redirectTo: `${url_origin}/api/auth/callback`,
+        },
+      });
+  
+      if (error) {
+        return new Response(error.message, { status: 500 });
+      }
+
+      return redirect(data.url);
     }
-
-    return redirect(data.url);
   }
 
-  if (!email || !password) {
-    return new Response("Email and password are required", { status: 400 });
-  }
+  // if (!email || !password) {
+  //   return new Response("Email and password are required", { status: 400 });
+  // }
 
-  const { data, error } = await supabase.auth.signInWithPassword({
-    email,
-    password,
-  });
+  // const { data, error } = await supabase.auth.signInWithPassword({
+  //   email,
+  //   password,
+  // });
 
-  if (error) {
-    return new Response(error.message, { status: 500 });
-  }
+  // if (error) {
+  //   return new Response(error.message, { status: 500 });
+  // }
 
-  const { access_token, refresh_token } = data.session;
-  cookies.set("sb-access-token", access_token, {
-    path: "/",
-  });
-  cookies.set("sb-refresh-token", refresh_token, {
-    path: "/",
-  });
+  // const { access_token, refresh_token } = data.session;
+  // cookies.set("sb-access-token", access_token, {
+  //   path: "/",
+  // });
+  // cookies.set("sb-refresh-token", refresh_token, {
+  //   path: "/",
+  // });
   return redirect("/dashboard");
 };
