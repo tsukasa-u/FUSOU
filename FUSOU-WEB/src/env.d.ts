fd37dda7f4c7c370a2a4a64b40015e23a17b3f13
Oguri Hideyuki <ogu.hide.u@gmail.com>
2025-04-29 03:21:43 +0900

    add public

--- FUSOU-WEB/src/env.d.ts
+++ FUSOU-WEB/src/env.d.ts
@@ -1,7 +1,7 @@
 interface ImportMetaEnv {
-    readonly SUPABASE_URL: string
-    readonly SUPABASE_ANON_KEY: string
-}
+    readonly PUBLIC_SUPABASE_URL: string
+    readonly PUBLIC_SUPABASE_ANON_KEY: string
+  }
   
 interface ImportMeta {
     readonly env: ImportMetaEnv
