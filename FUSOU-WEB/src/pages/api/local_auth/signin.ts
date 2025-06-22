90394cafec139a420e2689fa5fb124e8bb26f9e9
Oguri Hideyuki <68815462+tsukasa-u@users.noreply.github.com>
2025-06-23 04:51:41 +0900

    add local app sign in page on web & add metadata in KC API JSON file

---
+++ FUSOU-WEB/src/pages/api/local_auth/signin.ts
@@ -0,0 +1,54 @@
+import type { APIRoute } from "astro";
+import { supabase } from "../../../lib/supabase";
+import type { Provider } from "@supabase/supabase-js";
+
+export const POST: APIRoute = async ({ request, cookies, redirect }) => {
+  const formData = await request.formData();
+
+  const url_origin = import.meta.env.PUBLIC_SITE_URL;
+  // const url_origin = process.env.PUBLIC_SITE_URL;
+
+  const provider = formData.get("provider")?.toString();
+
+  const validProviders = ["google"];
+
+  if (provider && validProviders.includes(provider)) {
+    if (provider == "google") {
+      const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: "google",
+        options: {
+          scopes: "https://www.googleapis.com/auth/drive.file",
+          redirectTo: `${url_origin}/api/local_auth/callback`,
+          queryParams: {
+            prompt: "consent",
+            access_type: "offline",
+          },
+        },
+      });
+
+      if (error) {
+        return new Response(error.message, { status: 500 });
+      }
+      cookies.set("sb-provider", provider, {
+        path: "/",
+      });
+
+      return redirect(data.url);
+    } else {
+      const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: provider as Provider,
+        options: {
+          redirectTo: `${url_origin}/api/local_auth/callback`,
+        },
+      });
+
+      if (error) {
+        return new Response(error.message, { status: 500 });
+      }
+
+      return redirect(data.url);
+    }
+  }
+
+  return redirect("/returnLocalApp");
+};
