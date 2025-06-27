f76e572eb1b059e85f8ec6df9d51c79c3e62d4a6
Oguri Hideyuki <68815462+tsukasa-u@users.noreply.github.com>
2025-06-27 15:00:32 +0900

    Initial commit
    Project scaffolding and initial file setup.

--- FUSOU-PROXY/proxy_go/PAC_file.go
+++
@@ -1,3 +0,0 @@
-package main
-
-// this is for the PAC file with optional proxy setting
--- FUSOU-PROXY/proxy_go/util.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "bytes"
-
-func deep_copy_byte_buffer(src *bytes.Buffer) *bytes.Buffer {
-	dst := new(bytes.Buffer)
-	dst.Write(src.Bytes())
-	return dst
-}
