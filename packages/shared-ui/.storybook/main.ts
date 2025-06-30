5ed0dab802a5ba657b6edaf5a6e50c0c254fc4d2
Oguri Hideyuki <68815462+tsukasa-u@users.noreply.github.com>
2025-06-30 12:27:29 +0900

    test impl

--- packages/shared-ui/.storybook/main.ts
+++ packages/shared-ui/.storybook/main.ts
@@ -16,6 +16,7 @@ const config: StorybookConfig = {
     getAbsolutePath("@storybook/addon-docs"),
     getAbsolutePath("@storybook/addon-a11y"),
     getAbsolutePath("@storybook/addon-vitest"),
+    getAbsolutePath("@storybook/addon-themes"),
   ],
   framework: {
     name: getAbsolutePath("@storybook/web-components-vite"),
--- packages/shared-ui/.storybook/preview.ts
+++ packages/shared-ui/.storybook/preview.ts
@@ -1,14 +1,28 @@
-import type { Preview } from '@storybook/web-components-vite'
+import type { Preview } from "@storybook/web-components-vite";
+import "../src/index.css";
+import { withThemeByDataAttribute } from "@storybook/addon-themes";
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
 };
 
 export default preview;
+
+export const decorators = [
+  withThemeByDataAttribute({
+    themes: {
+      light: "light",
+      dark: "dark",
+      retro: "retro",
+    },
+    defaultTheme: "light",
+    attributeName: "data-mode",
+  }),
+];
