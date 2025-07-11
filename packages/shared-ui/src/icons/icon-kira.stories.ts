da12a0b9031688c1c8bd3bd877c62f133de13a77
Oguri Hideyuki <68815462+tsukasa-u@users.noreply.github.com>
2025-07-12 06:34:01 +0900

    add icon kira

---
+++ packages/shared-ui/src/icons/icon-kira.stories.ts
@@ -0,0 +1,53 @@
+import type { Meta, StoryObj } from "@storybook/web-components-vite";
+
+import type { IconKiraProps } from "./icon-kira";
+import { IconKiraBasic, IconKiraCatalog } from "./icon-kira";
+
+const size_list = ["full", "none", "xs", "sm", "md", "lg", "xl"];
+
+const meta = {
+  title: "FUSOU/icons/icon-kira",
+  tags: ["autodocs"],
+} satisfies Meta<IconKiraProps>;
+
+export default meta;
+type Story = StoryObj<IconKiraProps>;
+
+export const basic: Story = {
+  render: (args) => IconKiraBasic(args),
+  name: "Basic",
+  argTypes: {
+    kira_type: {
+      control: { type: "select" },
+      options: [1, 2, 3],
+    },
+    size: {
+      control: { type: "select" },
+      options: size_list,
+
+      table: {
+        defaultValue: { summary: "xs" },
+        type: {
+          summary: size_list.join("\|"),
+        },
+      },
+    },
+  },
+  args: {
+    kira_type: 1,
+    size: "full",
+  },
+};
+
+export const catalog: Story = {
+  render: (_) => IconKiraCatalog(),
+  name: "Catalog",
+  argTypes: {
+    kira_type: {
+      control: { disable: true },
+    },
+    size: {
+      control: { disable: true },
+    },
+  },
+};
