2b281cafc7667479c3577016cd30cd3f4721017b
Oguri Hideyuki <68815462+tsukasa-u@users.noreply.github.com>
2025-07-15 02:51:45 +0900

    change class_size

--- packages/shared-ui/src/icons/kira.ts
+++ packages/shared-ui/src/icons/kira.ts
@@ -10,10 +10,10 @@ export interface IconKiraProps {
 
 const class_size = {
   xs: "h-6",
-  sm: "h-8",
-  md: "h-12",
-  lg: "h-20",
-  xl: "h-36",
+  sm: "h-7.5",
+  md: "h-9",
+  lg: "h-11",
+  xl: "h-14",
   full: "h-full",
   none: "",
 };
