31e36f58326e9938b688ea4f3c8a204c7539eb8c
Oguri Hideyuki <68815462+tsukasa-u@users.noreply.github.com>
2025-08-11 21:28:57 +0900

    update equip_list

--- packages/FUSOU-APP/src/utility/get_data_set.tsx
+++ packages/FUSOU-APP/src/utility/get_data_set.tsx
@@ -4,9 +4,39 @@ import {
   useShips,
   useSlotItems,
 } from "./provider";
-import { MstShip, MstSlotItem, MstSlotItems } from "@ipc-bindings/get_data";
-import { Ship } from "@ipc-bindings/port";
-import { SlotItem, SlotItems } from "@ipc-bindings/require_info";
+import type {
+  MstShip,
+  MstSlotItem,
+  MstSlotItems,
+} from "@ipc-bindings/get_data";
+import type { Ship } from "@ipc-bindings/port";
+import type { SlotItem, SlotItems } from "@ipc-bindings/require_info";
+
+export const get_data_set_equip = (equip_ids: number[]) => {
+  const [slot_items] = useSlotItems();
+  const [mst_slot_items] = useMstSlotItems();
+  let ret: {
+    [key: number]: {
+      slot_item: SlotItem | undefined;
+      mst_slot_item: MstSlotItem | undefined;
+    };
+  } = {};
+  equip_ids.forEach((equip_id) => {
+    const slot_item = slot_items.slot_items[equip_id];
+
+    const mst_slot_item_id = slot_item?.slotitem_id;
+    const mst_slot_item = mst_slot_item_id
+      ? mst_slot_items.mst_slot_items[mst_slot_item_id]
+      : undefined;
+
+    ret[equip_id] = {
+      slot_item: slot_item,
+      mst_slot_item: mst_slot_item,
+    };
+  });
+
+  return ret;
+};
 
 export const get_data_set_ship = (ship_ids: number[]) => {
   const [ships] = useShips();
@@ -21,51 +51,45 @@ export const get_data_set_ship = (ship_ids: number[]) => {
       mst_slot_items: MstSlotItems | undefined;
     };
   } = {};
-  ship_ids.map((ship_id) => {
+  ship_ids.forEach((ship_id) => {
     const ship = ships.ships[ship_id];
 
-    const mst_ship_id = ship ? (ship.ship_id ?? undefined) : undefined;
+    const mst_ship_id = ship?.ship_id ?? undefined;
     const mst_ship = mst_ship_id ? mst_ships.mst_ships[mst_ship_id] : undefined;
 
-    const slot_item_ids = ship ? (ship.slot ?? undefined) : undefined;
-    const slot_item_id_ex = ship ? (ship.slot_ex ?? undefined) : undefined;
+    const slot_item_ids = ship?.slot ?? undefined;
+    const slot_item_id_ex = ship?.slot_ex ?? undefined;
     const slot_item_id_list = slot_item_ids
       ? slot_item_id_ex
         ? [...slot_item_ids, slot_item_id_ex]
         : [...slot_item_ids]
       : undefined;
-    const slot_item_list = slot_item_id_list
-      ? slot_item_id_list.map((id) => slot_items.slot_items[id])
-      : undefined;
-    const slot_item_map = slot_item_list
-      ? slot_item_list.reduce(
-          (dict, slot_item) => (
-            slot_item ? (dict[slot_item.id] = slot_item) : dict, dict
-          ),
-          {} as { [x: number]: SlotItem | undefined }
-        )
-      : undefined;
+    const slot_item_list = slot_item_id_list?.map(
+      (id) => slot_items.slot_items[id]
+    );
+    const slot_item_map = slot_item_list?.reduce(
+      (dict, slot_item) => (
+        slot_item ? (dict[slot_item.id] = slot_item) : dict, dict
+      ),
+      {} as { [x: number]: SlotItem | undefined }
+    );
 
     const mst_slot_item_list = slot_item_list
-      ? slot_item_list
-          .map((slot_item) =>
-            slot_item
-              ? mst_slot_items.mst_slot_items[slot_item.slotitem_id]
-              : undefined
-          )
-          .filter((slot_item) => slot_item)
-          .map((slot_item) => slot_item!)
-      : undefined;
-    const mst_slot_item_map = mst_slot_item_list
-      ? mst_slot_item_list.reduce(
-          (dict, slot_item) => (
-            slot_item ? (dict[slot_item.id] = slot_item) : dict, dict
-          ),
-          {} as { [x: number]: MstSlotItem | undefined }
-        )
-      : undefined;
+      ?.map((slot_item) =>
+        slot_item
+          ? mst_slot_items.mst_slot_items[slot_item.slotitem_id]
+          : undefined
+      )
+      .filter((slot_item) => slot_item)
+      .map((slot_item) => slot_item!);
+    const mst_slot_item_map = mst_slot_item_list?.reduce(
+      (dict, slot_item) => (
+        slot_item ? (dict[slot_item.id] = slot_item) : dict, dict
+      ),
+      {} as { [x: number]: MstSlotItem | undefined }
+    );
 
-    ret[Number(ship_id)] = {
+    ret[ship_id] = {
       ship: ship,
       mst_ship: mst_ship,
       slot_items: {
