dc3f038a741d96cdc441e74d9c12eaa3e73c9426
Oguri Hideyuki <68815462+tsukasa-u@users.noreply.github.com>
2025-10-18 01:21:07 +0900

    fix game screen size & version up

--- packages/FUSOU-APP/src-tauri/src/window/scripts/external_init_script.js
+++ packages/FUSOU-APP/src-tauri/src/window/scripts/external_init_script.js
@@ -1,4 +1,4 @@
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("load", () => {
   observer.observe(window.document.body, {
     subtree: true,
     childList: true,
@@ -9,41 +9,45 @@ window.addEventListener("DOMContentLoaded", () => {
   let html_element = document.getElementsByTagName("html")[0];
   if (html_element) html_element.setAttribute("style", "overflow: hidden;");
 
-  let dmm_ntgnavi = document.getElementsByClassName("dmm-ntgnavi")[0];
-  if (dmm_ntgnavi) dmm_ntgnavi.setAttribute("style", "display: none;");
+  let header = document.getElementsByTagName("header")[0];
+  console.log("header:", header);
+  if (header) header.setAttribute("style", "display: none;");
 
-  let foot = document.getElementById("foot");
-  if (foot) foot.setAttribute("style", "display: none;");
+  let footer = document.getElementsByTagName("footer")[0];
+  console.log("footer:", footer);
+  if (footer) footer.setAttribute("style", "display: none;");
 
-  let ntg_recommend = document.getElementById("ntg-recommend");
-  if (ntg_recommend) ntg_recommend.setAttribute("style", "display: none;");
-
-  let area_naviapp = document.getElementsByClassName("area-naviapp")[0];
-  if (area_naviapp) area_naviapp.setAttribute("style", "display: none;");
+  let aside = document.getElementsByTagName("aside")[0];
+  console.log("aside:", aside);
+  if (aside) aside.setAttribute("style", "display: none;");
 
   let game_frame = document.getElementById("game_frame");
+  const game_frame_height = 720;
+  const game_frame_width = 1200;
   if (game_frame) {
     game_frame.setAttribute(
       "style",
-      "position: fixed; margin: 0; margin-right: auto; margin-left: auto; top: -24px; left: -8px; transform-origin: 8px 24px;"
+      `position: fixed; margin: 0px 0px; top: 0px; left: 0px; transform-origin: 0px 0px; transform: scale(1.12083, 1.12083); width: ${game_frame_width}px;`
     );
     game_frame.style.transform =
       "scale(" +
-      window.innerHeight / 712 +
+      window.innerHeight / game_frame_height +
       ", " +
-      window.innerHeight / 712 +
+      window.innerHeight / game_frame_height +
       ")";
   }
 });
 
 window.addEventListener("resize", () => {
   let game_frame = document.getElementById("game_frame");
+  const game_frame_height = 720;
+  // const game_frame_width = 1200;
   if (game_frame)
     game_frame.style.transform =
       "scale(" +
-      window.innerHeight / 712 +
+      window.innerHeight / game_frame_height +
       ", " +
-      window.innerHeight / 712 +
+      window.innerHeight / game_frame_height +
       ")";
 });
 
