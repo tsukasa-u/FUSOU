172f987cda57825af6c302d23e4794a1b5bba109
Oguri Hideyuki <ogu.hide.u@gmail.com>
2025-11-16 13:54:54 +0900

    Adds feature flag to skip specific test data
    Implements a feature flag to selectively skip test data based on timestamp, specifically for tests related to data before a certain date.
    
    This change allows for running integration tests against different datasets based on a date condition, enabled via the `from20250627` feature.
    It introduces a mechanism to read timestamps from file content or derive them from the file name to determine whether to skip certain test cases.

--- packages/kc_api/crates/kc-api-integration-test/tests/check_database_field_size.rs
+++ packages/kc_api/crates/kc-api-integration-test/tests/check_database_field_size.rs
@@ -122,6 +122,48 @@ fn emit_data(emit_data: EmitData) -> Option<ReturnType> {
     None
 }
 
+
+fn get_timestamp_from_file_content(file_path: PathBuf) -> String {
+    let regex_timestamp = regex::Regex::new(r#"Timestamp: ([0-9]+)"#).unwrap();
+    let file_content = std::fs::read_to_string(file_path.clone())
+        .unwrap_or_else(|_| panic!("failed to read test data file: {}", file_path.display()));
+    let timestamp = file_content
+        .lines()
+        .find_map(|line| {
+            regex_timestamp
+                .captures(line)
+                .and_then(|caps| caps.get(1).map(|m| m.as_str().to_string()))
+        })
+        .unwrap_or({
+            let (prefix, _) = file_path
+                .file_name()
+                .unwrap_or_else(|| panic!("failed to get file name: {}", file_path.display()))
+                .to_str()
+                .unwrap_or_else(|| {
+                    panic!(
+                        "failed to convert file name to str: {}",
+                        file_path.display()
+                    )
+                })
+                .split_once("@")
+                .unwrap_or_else(|| panic!("failed to split file name: {}", file_path.display()));
+            let splited_under = match prefix.split_once("_") {
+                Some((_, suffix)) => suffix.to_string(),
+                None => prefix.to_string(),
+            };
+
+            if splited_under.ends_with('S') || splited_under.ends_with('Q') {
+                splited_under[..splited_under.len() - 1].to_string()
+            } else {
+                panic!(
+                    "failed to get timestamp from file name: {}",
+                    file_path.display()
+                )
+            }
+        });
+    timestamp
+}
+
 pub fn check_database_field_size() {
     use regex::Regex;
 
@@ -158,6 +200,31 @@ pub fn check_database_field_size() {
         std::collections::HashMap::new();
 
     for file_api_seq in file_api_seq_vec {
+        let skip_flag = file_api_seq
+            .iter()
+            .filter(|file_path| {
+                let file_name = file_path
+                    .file_name()
+                    .expect("failed to get file name")
+                    .to_str()
+                    .expect("failed to convert to str");
+                file_name.contains("@api_start2@getData")
+            })
+            .any(|file_path| {
+                let ts_int = get_timestamp_from_file_content(file_path.clone()).parse::<i64>() .expect("failed to get timestamp from file_ contnet");
+                #[cfg(feature = "from20250627")]
+                {
+                    !(ts_int >= 1750993200)
+                }
+                #[cfg(not(feature = "from20250627"))]
+                {
+                    !(ts_int < 1750993200)
+                }
+            });
+        if skip_flag {
+            continue;
+        }
+            
         for file_path in file_api_seq {
             let file_content = fs::read_to_string(file_path.clone())
                 .unwrap_or_else(|_| panic!("can not read the file({})", file_path.display()));
@@ -187,7 +254,7 @@ pub fn check_database_field_size() {
                 s if s.ends_with("S") => {
                     let emit_data_list: Vec<EmitData> =
                         parser::response_parser(path_name, data_removed_metadata).unwrap_or_else(
-                            |_| panic!("failed to parse the file({})", file_path.display()),
+                            |e| panic!("failed to parse the file({}), e: {e}", file_path.display()),
                         );
                     emit_data_list
                 }
@@ -196,9 +263,9 @@ pub fn check_database_field_size() {
                         .replace("%5B", "[")
                         .replace("%5D", "]");
                     let emit_data_list: Vec<EmitData> =
-                        parser::request_parser(path_name, encoded_data).unwrap_or_else(|_| {
-                            panic!("failed to parse the file({})", file_path.display())
-                        });
+                        parser::request_parser(path_name, encoded_data).unwrap_or_else(
+                            |e| panic!("failed to parse the file({}), e: {e}", file_path.display()),
+                        );
                     emit_data_list
                 }
                 _ => {
