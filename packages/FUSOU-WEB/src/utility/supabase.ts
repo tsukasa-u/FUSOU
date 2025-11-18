d71dc547fdb5896f926ba2d7effd8e117ae80ca0
Oguri Hideyuki <68815462+tsukasa-u@users.noreply.github.com>
2025-11-18 11:29:10 +0900

    Configures Supabase client for the application
    Initializes the Supabase client with environment variables and implements function to retrieve user refresh tokens.
    This enhances the application's capability to manage user authentication and access control.

--- packages/FUSOU-WEB/src/utility/supabase.ts
+++ packages/FUSOU-WEB/src/utility/supabase.ts
@@ -1,27 +1,27 @@
-
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  import.meta.env.SUPABASE_URL, 
-  import.meta.env.SUPABASE_ANON_KEY!
+  import.meta.env.PUBLIC_SUPABASE_URL,
+  import.meta.env.PUBLIC_SUPABASE_ANON_KEY!
 );
 
 export const getRefreshToken = async (user_id: string) => {
-  const { data, error } = await supabase.from('users')
-    .select('provider_refresh_token')
-    .eq('id', user_id)
+  const { data, error } = await supabase
+    .from("users")
+    .select("provider_refresh_token")
+    .eq("id", user_id)
     .single()
     .then(({ data, error }) => {
       if (error) {
-        console.error('Error selecting refresh token:', error);
+        console.error("Error selecting refresh token:", error);
       } else {
-        console.log('Refresh token selected successfully:', data);
+        console.log("Refresh token selected successfully:", data);
       }
       return { data, error };
     });
   if (error) {
-    console.error('Error selecting refresh token:', error);
+    console.error("Error selecting refresh token:", error);
     return null;
   }
   return data?.provider_refresh_token;
-}
+};
