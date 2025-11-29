323f83df090b70c1bc70b7e5e789afd163e39de5
Oguri Hideyuki <ogu.hide.u@gmail.com>
2025-11-30 01:49:50 +0900

    fix

--- packages/FUSOU-WEB/src/pages/api/asset-sync/types.ts
+++ packages/FUSOU-WEB/src/pages/api/asset-sync/types.ts
@@ -9,6 +9,8 @@ export type AssetKeyCache = {
 
 export type D1Row = Record<string, unknown>;
 
+export type D1Result = Record<string, unknown>;
+
 export type D1AllResult = {
   results?: D1Row[];
 };
@@ -17,6 +19,7 @@ export interface D1Statement {
   bind(...args: unknown[]): D1Statement;
   run(): Promise<unknown>;
   all?(): Promise<D1AllResult>;
+  first?(): Promise<D1Result | undefined>;
 }
 
 export interface D1Database {
