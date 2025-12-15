9d6f4ce8396180f702bb28678e0d52aabb87d973
Oguri Hideyuki <ogu.hide.u@gmail.com>
2025-12-16 05:08:16 +0900

    Enables compact endpoint via Pages Function
    Configures a Cloudflare Pages Function to handle requests to the /api/compact endpoint.
    This change allows the compact functionality to be accessed through a serverless function.
    Also updates the wrangler config to set the pages build output directory.

--- packages/FUSOU-WEB/functions/_worker.ts
+++ packages/FUSOU-WEB/functions/_worker.ts
@@ -3,5 +3,11 @@
  * Exports Durable Object classes for use in the project
  */
 
+// This file is required to enable Durable Objects in Pages Functions
+// Durable Object classes must be exported from the worker entry point
+
 // Export Durable Object classes
 export { CompactorDO } from '../src/server/durable-objects/CompactorDO';
+
+// Re-export Pages Functions handlers
+export { onRequest as onRequestCompact } from './api/compact';
