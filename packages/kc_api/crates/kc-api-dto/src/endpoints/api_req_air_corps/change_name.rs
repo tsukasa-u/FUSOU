0865df014ac8856993e4171b5b54f80c867a190d
Oguri Hideyuki <68815462+tsukasa-u@users.noreply.github.com>
2026-09-11 01:11:10 +0900

    feat(kc-api-dto): add missing new endpoint DTO implementations
    These files were referenced by mod.rs in the previous commit but omitted:
    - api_get_member/preset_dev_items.rs: GET /api_get_member/preset_dev_items
    - api_req_air_corps/change_name.rs: POST /api_req_air_corps/change_name
    - api_req_practice/change_matching_kind.rs: POST /api_req_practice/change_matching_kind

---
+++ packages/kc_api/crates/kc-api-dto/src/endpoints/api_req_air_corps/change_name.rs
@@ -0,0 +1,98 @@
+#![doc = "# kanColle API"]
+#![doc = "KC APIs are also dependent on kcapi::kcapi_common."]
+#![doc = "The dependency graph of the APIs is shown below."]
+#![doc = register_trait::insert_svg!(path="../../tests/struct_dependency_svg/api_req_air_corps@change_name.svg", id="kc-dependency-svg-embed", style="border: 1px solid black; height:80vh; width:100%", role="img", aria_label="KC_API_dependency(api_req_air_corps/change_name)")]
+#![doc = include_str!("../../../../../js/svg_pan_zoom.html")]
+
+use serde::{Deserialize, Serialize};
+
+use register_trait::{add_field, register_struct};
+use register_trait::{FieldSizeChecker, QueryWithExtra, TraitForRoot, TraitForTest};
+
+#[derive(FieldSizeChecker, TraitForTest, TraitForRoot)]
+#[struct_test_case(field_extra, type_value, integration)]
+#[add_field(extra_for_qs)]
+#[derive(Debug, Clone, QueryWithExtra)]
+pub struct Req {
+    #[qs(rename = "api_token")]
+    pub api_token: String,
+    #[qs(rename = "api_verno")]
+    pub api_verno: i64,
+    #[qs(rename = "api_area_id")]
+    pub api_area_id: i64,
+    #[qs(rename = "api_base_id")]
+    pub api_base_id: i64,
+    #[qs(rename = "api_name")]
+    pub api_name: String,
+}
+
+#[derive(FieldSizeChecker, TraitForTest, TraitForRoot)]
+#[struct_test_case(field_extra, type_value, integration)]
+#[add_field(extra_with_flatten)]
+#[register_struct(name = "api_req_air_corps/change_name")]
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Res {
+    #[serde(rename = "api_result")]
+    pub api_result: i64,
+    #[serde(rename = "api_result_msg")]
+    pub api_result_msg: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::test_utils::struct_normalize::{glob_match_normalize, FormatType};
+    use dotenvy::dotenv;
+    use register_trait::simple_root_test;
+
+    use super::*;
+    #[test]
+    fn test_deserialize() {
+        dotenv().expect(".env file not found");
+        let target_path = std::env::var("TEST_DATA_PATH").expect("failed to get env data");
+
+        let pattern_str = "S@api_req_air_corps@change_name";
+        let log_path = "./src/endpoints/api_req_air_corps/change_name@S.log";
+        simple_root_test::<Res>(
+            target_path.clone(),
+            pattern_str.to_string(),
+            log_path.to_string(),
+        );
+
+        let pattern_str = "Q@api_req_air_corps@change_name";
+        let log_path = "./src/endpoints/api_req_air_corps/change_name@Q.log";
+        simple_root_test::<Req>(
+            target_path.clone(),
+            pattern_str.to_string(),
+            log_path.to_string(),
+        );
+    }
+    #[test]
+    fn test_organize_test_data() {
+        dotenv().expect(".env file not found");
+        let target_path = std::env::var("TEST_DATA_PATH").expect("failed to get env data");
+        let snap_file_path = std::env::var("TEST_DATA_REPO_PATH").expect("failed to get env data");
+
+        let req_and_res_pattern_str = "@api_req_air_corps@change_name";
+        let snap_path = format!("{snap_file_path}/kcsapi");
+        let log_path = "./src/endpoints/api_req_air_corps/change_name@snap_data@S.log";
+        glob_match_normalize::<Req, Res>(
+            target_path.clone(),
+            req_and_res_pattern_str.to_string(),
+            snap_path.to_string(),
+            FormatType::Json,
+            log_path.to_string(),
+            None,
+        );
+
+        let log_path = "./src/endpoints/api_req_air_corps/change_name@snap_data@Q.log";
+        glob_match_normalize::<Req, Res>(
+            target_path.clone(),
+            req_and_res_pattern_str.to_string(),
+            snap_path.to_string(),
+            FormatType::QueryString,
+            log_path.to_string(),
+            None,
+        );
+    }
+}
---
+++ packages/kc_api/crates/kc-api-dto/src/endpoints/api_req_practice/change_matching_kind.rs
@@ -0,0 +1,106 @@
+#![doc = "# kanColle API"]
+#![doc = "KC APIs are also dependent on kcapi::kcapi_common."]
+#![doc = "The dependency graph of the APIs is shown below."]
+#![doc = register_trait::insert_svg!(path="../../tests/struct_dependency_svg/api_req_practice@change_matching_kind.svg", id="kc-dependency-svg-embed", style="border: 1px solid black; height:80vh; width:100%", role="img", aria_label="KC_API_dependency(api_req_practice/change_matching_kind)")]
+#![doc = include_str!("../../../../../js/svg_pan_zoom.html")]
+
+use serde::{Deserialize, Serialize};
+
+use register_trait::{add_field, register_struct};
+use register_trait::{FieldSizeChecker, QueryWithExtra, TraitForRoot, TraitForTest};
+
+#[derive(FieldSizeChecker, TraitForTest, TraitForRoot)]
+#[struct_test_case(field_extra, type_value, integration)]
+#[add_field(extra_for_qs)]
+#[derive(Debug, Clone, QueryWithExtra)]
+pub struct Req {
+    #[qs(rename = "api_token")]
+    pub api_token: String,
+    #[qs(rename = "api_verno")]
+    pub api_verno: i64,
+    #[qs(rename = "api_selected_kind")]
+    pub api_selected_kind: i64,
+}
+
+#[derive(FieldSizeChecker, TraitForTest, TraitForRoot)]
+#[struct_test_case(field_extra, type_value, integration)]
+#[add_field(extra_with_flatten)]
+#[register_struct(name = "api_req_practice/change_matching_kind")]
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Res {
+    #[serde(rename = "api_result")]
+    pub api_result: i64,
+    #[serde(rename = "api_result_msg")]
+    pub api_result_msg: String,
+    #[serde(rename = "api_data")]
+    pub api_data: Option<ApiData>,
+}
+
+#[derive(FieldSizeChecker, TraitForTest)]
+#[struct_test_case(field_extra, type_value, integration)]
+#[add_field(extra_with_flatten)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ApiData {
+    #[serde(rename = "api_update_flag")]
+    pub api_update_flag: i64,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::test_utils::struct_normalize::{glob_match_normalize, FormatType};
+    use dotenvy::dotenv;
+    use register_trait::simple_root_test;
+
+    use super::*;
+    #[test]
+    fn test_deserialize() {
+        dotenv().expect(".env file not found");
+        let target_path = std::env::var("TEST_DATA_PATH").expect("failed to get env data");
+
+        let pattern_str = "S@api_req_practice@change_matching_kind";
+        let log_path = "./src/endpoints/api_req_practice/change_matching_kind@S.log";
+        simple_root_test::<Res>(
+            target_path.clone(),
+            pattern_str.to_string(),
+            log_path.to_string(),
+        );
+
+        let pattern_str = "Q@api_req_practice@change_matching_kind";
+        let log_path = "./src/endpoints/api_req_practice/change_matching_kind@Q.log";
+        simple_root_test::<Req>(
+            target_path.clone(),
+            pattern_str.to_string(),
+            log_path.to_string(),
+        );
+    }
+    #[test]
+    fn test_organize_test_data() {
+        dotenv().expect(".env file not found");
+        let target_path = std::env::var("TEST_DATA_PATH").expect("failed to get env data");
+        let snap_file_path = std::env::var("TEST_DATA_REPO_PATH").expect("failed to get env data");
+
+        let req_and_res_pattern_str = "@api_req_practice@change_matching_kind";
+        let snap_path = format!("{snap_file_path}/kcsapi");
+        let log_path = "./src/endpoints/api_req_practice/change_matching_kind@snap_data@S.log";
+        glob_match_normalize::<Req, Res>(
+            target_path.clone(),
+            req_and_res_pattern_str.to_string(),
+            snap_path.to_string(),
+            FormatType::Json,
+            log_path.to_string(),
+            None,
+        );
+
+        let log_path = "./src/endpoints/api_req_practice/change_matching_kind@snap_data@Q.log";
+        glob_match_normalize::<Req, Res>(
+            target_path.clone(),
+            req_and_res_pattern_str.to_string(),
+            snap_path.to_string(),
+            FormatType::QueryString,
+            log_path.to_string(),
+            None,
+        );
+    }
+}
